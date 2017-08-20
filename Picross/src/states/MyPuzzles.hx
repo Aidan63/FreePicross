5cda4708695523da83e0a51dc7993782bdd98d0f
Aidan Lee <aidan.lee63@gmail.com>
2017-08-20 18:00:44 +0100

    Added two buttons and switching panels when items in the grid are clicked.

--- Picross/src/states/MyPuzzles.hx
+++ Picross/src/states/MyPuzzles.hx
@@ -3,15 +3,25 @@ package states;
 import luxe.States;
 import luxe.Visual;
 import luxe.Color;
+import luxe.Vector;
+import luxe.Rectangle;
 import luxe.Parcel;
 import luxe.ParcelProgress;
+import ui.Button;
+import ui.GridView;
 
 class MyPuzzles extends State
 {
     private var parcel : Parcel;
     private var listenPuzzleSelected : String;
 
-    private var gridView : Visual;
+    private var gridView : GridView;
+    private var bttnCreate : Button;
+    private var bttnHome : Button;
+
+    private var panel1 : Visual;
+    private var panel2 : Visual;
+    private var activePanel : Visual;
 
     override public function onenter<T>(_data : T)
     {
@@ -48,27 +58,88 @@ class MyPuzzles extends State
     {
         gridView.events.unlisten(listenPuzzleSelected);
         gridView.destroy();
+
+        bttnHome.destroy();
+        bttnCreate.destroy();
+        panel1.destroy();
     }
 
     private function assets_loaded(_parcel : Parcel)
     {
-        gridView = new ui.GridView({
-            boundary : new luxe.Rectangle(40, 40, 580, 540),
-            columns : 4,
+        gridView = new GridView({
+            boundary : new Rectangle(40, 40, 580, 540),
+            columns  : 4,
             x_offset : 20, y_offset : 20,
-            x_sep : 20, y_sep : 20,
-            items : [ for (i in 0...5) new Visual({
-                size : new luxe.Vector(120, 120),
-                color : luxe.Color.random(),
+            x_sep    : 20, y_sep    : 20,
+            items : [ for (i in 0...17) new Visual({
+                size : new Vector(120, 120),
+                color : Color.random(),
                 depth : 3
             }) ]
         });
+
+        var labelPos = [ new Vector(240, 30), new Vector(240, 30), new Vector(240, 50) ];
+        var labelCol = [ new Color(0.54, 0.54, 0.54, 1), new Color(0.54, 0.54, 0.54, 1), new Color(0.54, 0.54, 0.54, 1) ];
+        var texCol = [ new Color(0.86, 0.86, 0.86, 1), new Color(0.72, 0.72, 0.72, 1), new Color(0.72, 0.72, 0.72, 1) ];
+        var texUVs = [ new luxe.Rectangle(0  , 0, 80, 80), new luxe.Rectangle(80 , 0, 80, 80), new luxe.Rectangle(160, 0, 80, 80) ];
+
+        bttnHome = new Button({
+            name : 'bttnHome',
+            texture : Luxe.resources.texture('assets/images/ui/roundedButton.png'),
+            depth : 3,
+            pos : new Vector(40, 600),
+            size : new Vector(80, 80),
+            top : 30, left : 30, bottom : 30, right : 30,
+            labelOffsets : labelPos,
+            labelColors : labelCol,
+            textureUVs : texUVs,
+            textureColors : texCol
+        });
+
+        bttnCreate = new Button({
+            name : 'bttnCreate',
+            texture : Luxe.resources.texture('assets/images/ui/roundedButton.png'),
+            depth : 3,
+            pos : new Vector(140, 600),
+            size : new Vector(480, 80),
+            top : 30, left : 30, bottom : 30, right : 30,
+            labelOffsets : labelPos,
+            labelColors : labelCol,
+            textureUVs : texUVs,
+            textureColors : texCol,
+            label : new luxe.Text({
+                name : 'bttnCreateText',
+                text : 'Create',
+                point_size : 32,
+                depth : 4,
+                align : center,
+                align_vertical : center
+            })
+        });
+
+        panel1 = ui.creators.MyPuzzles.previewPanel();
+        panel2 = ui.creators.MyPuzzles.previewPanel();
+
+        panel1.pos.set_xy(660, -640);
+        panel2.pos.set_xy(660, -640);
         
         listenPuzzleSelected = gridView.events.listen('item.clicked', onItemSelected);
     }
 
     private function onItemSelected(_number : Int)
     {
-        trace(_number);
+        if (activePanel == null)
+        {
+            activePanel = panel1;
+            luxe.tween.Actuate.tween(activePanel.pos, 0.25, { y : 40 });
+        }
+        else
+        {
+            luxe.tween.Actuate.tween(activePanel.pos, 0.25, { y : 720 });
+            activePanel.id == panel1.id ? activePanel = panel2 : activePanel = panel1;
+
+            activePanel.pos.set_xy(660, -640);
+            luxe.tween.Actuate.tween(activePanel.pos, 0.25, { y : 40 });
+        }
     }
 }
