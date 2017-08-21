51b7ea0b1762d591ce8df960e6aed02112437805
Aidan Lee <aidan.lee63@gmail.com>
2017-08-21 12:24:25 +0100

    Puzzle preview panels now have play and remove buttons.

--- Picross/src/ui/creators/MyPuzzles.hx
+++ Picross/src/ui/creators/MyPuzzles.hx
@@ -1,6 +1,7 @@
 package ui.creators;
 
 import luxe.Vector;
+import luxe.Text;
 import luxe.Visual;
 import luxe.NineSlice;
 import luxe.Color;
@@ -17,6 +18,82 @@ class MyPuzzles
             color : new Color().rgb(0x333333),
         });
         panel.create(new Vector(660, 40), 580, 640);
+
+        new Text({
+            parent : panel,
+            name : 'title',
+            point_size : 64,
+            text : '',
+            pos : new Vector(20, 60),
+            align : left,
+            align_vertical : center
+        });
+
+        new Visual({
+            parent : panel,
+            name : 'preview',
+            pos : new Vector(40, 160),
+            size : new Vector(200, 200)
+        });
+
+        new Text({
+            parent : panel,
+            name : 'description',
+            point_size : 32,
+            text : '',
+            pos : new Vector(40, 420),
+            align : left,
+            align_vertical : center
+        });
+
+        // Add play and remove buttons.
+        var labelPos = [ new Vector(270, 20), new Vector(270, 20), new Vector(270, 40) ];
+        var labelCol = [ new Color(0.54, 0.54, 0.54, 1), new Color(0.54, 0.54, 0.54, 1), new Color(0.54, 0.54, 0.54, 1) ];
+        var texCol = [ new Color(0.86, 0.86, 0.86, 1), new Color(0.72, 0.72, 0.72, 1), new Color(0.72, 0.72, 0.72, 1) ];
+        var texUVs = [ new luxe.Rectangle(0  , 0, 80, 80), new luxe.Rectangle(80 , 0, 80, 80), new luxe.Rectangle(160, 0, 80, 80) ];
+
+        new Button({
+            parent  : panel,
+            name    : 'bttnPlay',
+            texture : Luxe.resources.texture('assets/images/ui/roundedButton.png'),
+            depth   : 3,
+            pos     : new Vector(20, 480),
+            size    : new Vector(540, 60),
+            top : 30, left : 30, bottom : 30, right : 30,
+            labelOffsets  : labelPos,
+            labelColors   : labelCol,
+            textureUVs    : texUVs,
+            textureColors : texCol,
+            label : new luxe.Text({
+                name : 'bttnPlayText',
+                text : 'Play',
+                point_size : 24,
+                depth : 4,
+                align : center,
+                align_vertical : center
+            })
+        });
+        new Button({
+            parent  : panel,
+            name    : 'bttnDelete',
+            texture : Luxe.resources.texture('assets/images/ui/roundedButton.png'),
+            depth   : 3,
+            pos     : new Vector(20, 560),
+            size    : new Vector(540, 60),
+            top : 30, left : 30, bottom : 30, right : 30,
+            labelOffsets  : labelPos,
+            labelColors   : labelCol,
+            textureUVs    : texUVs,
+            textureColors : texCol,
+            label : new luxe.Text({
+                name : 'bttnDeleteText',
+                text : 'Delete',
+                point_size : 24,
+                depth : 4,
+                align : center,
+                align_vertical : center
+            })
+        });
 
         return panel;
     }
