15b0733b1335c684cb082f59460c78d3d1f48851
Aidan Lee <aidan.lee63@gmail.com>
2017-08-21 12:14:51 +0100

    Puzzle preview now contains information on the selected puzzle.

--- Picross/src/states/MyPuzzles.hx
+++ Picross/src/states/MyPuzzles.hx
@@ -2,6 +2,7 @@ package states;
 
 import luxe.States;
 import luxe.Visual;
+import luxe.Text;
 import luxe.Color;
 import luxe.Vector;
 import luxe.Rectangle;
@@ -9,19 +10,64 @@ import luxe.Parcel;
 import luxe.ParcelProgress;
 import ui.Button;
 import ui.GridView;
+import data.PuzzleInfo;
+import phoenix.Texture;
+
+using utils.EntityHelper;
 
 class MyPuzzles extends State
 {
+    /**
+     *  The parcel which will load all required assets for this scene.
+     */
     private var parcel : Parcel;
-    private var listenPuzzleSelected : String;
 
+    /**
+     *  The grid which will hold all of the puzzle preview icons.
+     */
     private var gridView : GridView;
+
+    /**
+     *  Button which will open a popup to create a new puzzle.
+     */
     private var bttnCreate : Button;
+
+    /**
+     *  Button which will return the user to the main menu.
+     */
     private var bttnHome : Button;
 
+    /**
+     *  The first preview panel.
+     */
     private var panel1 : Visual;
+
+    /**
+     *  The second preview panel.
+     */
     private var panel2 : Visual;
+
+    /**
+     *  Points to the current preview panel.
+     */
     private var activePanel : Visual;
+
+    /**
+     *  All of textures for the puzzles.
+     */
+    private var puzzleTextures : Array<Texture>;
+
+    /**
+     *  All of the loaded user puzzles.
+     */
+    private var ugPuzzles : Array<PuzzleInfo>;
+
+    /**
+     *  The currently selected puzzle.
+     */
+    private var selectedID : Int;
+
+    private var listenPuzzleSelected : String;
 
     override public function onenter<T>(_data : T)
     {
@@ -52,6 +98,11 @@ class MyPuzzles extends State
         });
 
         parcel.load();
+
+        ugPuzzles = [
+            utils.storage.PuzzleStorage.load('strawberry'),
+            utils.storage.PuzzleStorage.load('277008110')
+        ];
     }
 
     override public function onleave<T>(_data : T)
@@ -62,20 +113,44 @@ class MyPuzzles extends State
         bttnHome.destroy();
         bttnCreate.destroy();
         panel1.destroy();
+        panel2.destroy();
     }
 
     private function assets_loaded(_parcel : Parcel)
     {
+        // Generate textures from all of the loaded user puzzles.
+        puzzleTextures = new Array<Texture>();
+        for (puzzle in ugPuzzles)
+        {
+            var tex = new Texture({
+                id : 'puzzleTexture-${puzzle.id}',
+                width : puzzle.grid.data[0].length,
+                height : puzzle.grid.data.length,
+                pixels : puzzle.pixels
+            });
+            tex.filter_min = tex.filter_mag = nearest;
+
+            puzzleTextures.push(tex);
+        }
+
+        // Generate preview icons from those textures for the grid.
+        var previews = new Array<Visual>();
+        for (tex in puzzleTextures)
+        {
+            previews.push(new Visual({
+                size : new Vector(120, 120),
+                texture : tex,
+                depth : 3
+            }));
+        }
+
+        // Creates the grid view, back, and create buttons.
         gridView = new GridView({
             boundary : new Rectangle(40, 40, 580, 540),
             columns  : 4,
             x_offset : 20, y_offset : 20,
             x_sep    : 20, y_sep    : 20,
-            items : [ for (i in 0...17) new Visual({
-                size : new Vector(120, 120),
-                color : Color.random(),
-                depth : 3
-            }) ]
+            items : previews
         });
 
         var labelPos = [ new Vector(240, 30), new Vector(240, 30), new Vector(240, 50) ];
@@ -117,29 +192,59 @@ class MyPuzzles extends State
             })
         });
 
+        // Create the preview panels and place them offscreen.
         panel1 = ui.creators.MyPuzzles.previewPanel();
         panel2 = ui.creators.MyPuzzles.previewPanel();
 
         panel1.pos.set_xy(660, -640);
         panel2.pos.set_xy(660, -640);
         
+        // Connect listeners.
         listenPuzzleSelected = gridView.events.listen('item.clicked', onItemSelected);
     }
 
+    /**
+     *  Moves in the panel which is not currently active and set that one to the active panel.
+     *  @param _number - 
+     */
     private function onItemSelected(_number : Int)
     {
         if (activePanel == null)
         {
+            // Move in puzzle for the first time.
             activePanel = panel1;
             luxe.tween.Actuate.tween(activePanel.pos, 0.25, { y : 40 });
         }
         else
         {
+            // If a new puzzle has been clicked swap the two puzzle panels.
+            if (_number == selectedID) return;
+
             luxe.tween.Actuate.tween(activePanel.pos, 0.25, { y : 720 });
             activePanel.id == panel1.id ? activePanel = panel2 : activePanel = panel1;
 
             activePanel.pos.set_xy(660, -640);
             luxe.tween.Actuate.tween(activePanel.pos, 0.25, { y : 40 });
         }
+
+        // Set the now active panels details.
+        selectedID = _number;
+        updateActivePanel(_number);
+    }
+
+    /**
+     *  Set the active panels name, description and preview based of the puzzle index provided.
+     *  @param _puzzleID The position in the puzzle info and puzzle texture array to get data from.
+     */
+    private function updateActivePanel(_puzzleID : Int)
+    {
+        var title       : Text   = cast activePanel.findChild('title');
+        var description : Text   = cast activePanel.findChild('description');
+        var preview     : Visual = cast activePanel.findChild('preview');
+
+        var puzzle : PuzzleInfo = ugPuzzles[_puzzleID];
+        title.text = puzzle.name;
+        description.text = puzzle.description;
+        preview.texture = puzzleTextures[_puzzleID];
     }
 }
