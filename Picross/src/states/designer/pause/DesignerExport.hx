6b70c9caa16e0e7c9407a56beaa954faa08f5982
Aidan Lee <aidan.lee63@gmail.com>
2017-08-24 12:56:38 +0100

    Updated the export to use the new TextEdit components.

--- Picross/src/states/designer/pause/DesignerExport.hx
+++ Picross/src/states/designer/pause/DesignerExport.hx
@@ -2,6 +2,8 @@ package states.designer.pause;
 
 import luxe.States;
 import luxe.Visual;
+import luxe.Input;
+import luxe.Text;
 
 using utils.EntityHelper;
 
@@ -20,32 +22,73 @@ class DesignerExport extends State
 
     override public function onenter<T>(_data : T)
     {
+        // Move the popup in and connect event listeners.
         luxe.tween.Actuate.tween(menu.pos, 0.25, { y : 80 }).ease(luxe.tween.easing.Quad.easeInOut);
 
-        listenExport = menu.findChild('ui_designer_export_bttnExport').events.listen('clicked', onExportClicked);
-        listenCancel = menu.findChild('ui_designer_export_bttnCancel').events.listen('clicked', onCancelClicked);
+        listenExport = menu.findChild('bttn_export').events.listen('released', onExportClicked);
+        listenCancel = menu.findChild('bttn_cancel').events.listen('released', onCancelClicked);
     }
 
     override public function onleave<T>(_data : T)
     {
+        // Move the popup out and remove event listeners.
         luxe.tween.Actuate.tween(menu.pos, 0.25, { y : 720 }).ease(luxe.tween.easing.Quad.easeInOut);
 
-        menu.findChild('ui_designer_export_bttnExport').events.unlisten(listenExport);
-        menu.findChild('ui_designer_export_bttnCancel').events.unlisten(listenCancel);
+        menu.findChild('bttn_export').events.unlisten(listenExport);
+        menu.findChild('bttn_cancel').events.unlisten(listenCancel);
     }
 
     override public function ondestroy()
     {
         menu.destroy();
     }
+
+    // Mouse events for the text edit input.
+    override public function onmousedown(_event : MouseEvent)
+    {
+        var mouse = Luxe.camera.screen_point_to_world(_event.pos);
+
+        var panelName : Visual = cast menu.findChild('panel_textedit_name');
+        var panelDesc : Visual = cast menu.findChild('panel_textedit_description');
+        var editName : Text = cast menu.findChild('textedit_name');
+        var editDesc : Text = cast menu.findChild('textedit_description');
+
+        if (editName.has('text_edit')) editName.remove('text_edit');
+        if (editDesc.has('text_edit')) editDesc.remove('text_edit');
+
+        if (mouse.pointInside(panelName.transform.world.pos, panelName.size))
+        {
+            if (!editName.has('text_edit'))
+            {
+                editName.add(new components.ui.TextEdit());
+            }
+        }
+        if (mouse.pointInside(panelDesc.transform.world.pos, panelDesc.size))
+        {
+            if (!editDesc.has('text_edit'))
+            {
+                editDesc.add(new components.ui.TextEdit());
+            }
+        }
+    }
+
+    /**
+     *  When export is clicked get the text from the name and description input and fire an event containing them.
+     *  @param _ - 
+     */
     private function onExportClicked(_)
     {
-        var nameInput : ui.TextInput = cast menu.findChild('ui_designer_export_inputName');
-        var descInput : ui.TextInput = cast menu.findChild('ui_designer_export_inputDescription');
+        var nameInput : Text = cast menu.findChild('textedit_name');
+        var descInput : Text = cast menu.findChild('textedit_description');
         Luxe.events.queue('designer.menu.export', { name : nameInput.text, description : descInput.text });
 
         machine.unset();
     }
+
+    /**
+     *  Change back to the pause menu.
+     *  @param _ - 
+     */
     private function onCancelClicked(_)
     {
         machine.set('menu');
