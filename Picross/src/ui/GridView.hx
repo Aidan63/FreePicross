e917c75f3abb56b87ca3da4a4f031444d609769d
Aidan Lee <aidan.lee63@gmail.com>
2017-08-24 15:10:45 +0100

    Removed ondestroy event to prevent crashes when destroying the grid view.

--- Picross/src/ui/GridView.hx
+++ Picross/src/ui/GridView.hx
@@ -97,15 +97,6 @@ class GridView extends Visual
         batcher.on(postrender, after);
     }
 
-    override public function ondestroy()
-    {
-        batcher.destroy(true);
-        targetTexture.destroy();
-
-        items = null;
-        background = null;
-    }
-
     /**
      *  Moves all items up or down depending on what way the mouse wheel moved.
      *  @param _event MouseEvent.
