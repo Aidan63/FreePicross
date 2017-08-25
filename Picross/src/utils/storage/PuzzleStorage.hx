e733ce26e7a6be7d737358bcf276e47e71f00449
Aidan Lee <aidan.lee63@gmail.com>
2017-08-25 12:25:06 +0100

    Added PuzzleStorage method for getting all puzzles found the in assets/puzzles folder.

--- Picross/src/utils/storage/PuzzleStorage.hx
+++ Picross/src/utils/storage/PuzzleStorage.hx
@@ -3,22 +3,57 @@ package utils.storage;
 import haxe.io.Bytes;
 import haxe.io.Path;
 import data.PuzzleInfo;
+import sys.FileSystem;
+import sys.io.File;
 
 class PuzzleStorage
 {
+    /**
+     *  Returns an absolute path to the puzzle storage.
+     *  @return String
+     */
     public static function puzzleStorage() : String
     {
         return Path.join([ Path.directory(Sys.programPath()), 'assets', 'puzzles' ]);
     }
 
-    public static function save(_info : PuzzleInfo)
+    /**
+     *  Returns every puzzle found in the assets/puzzles folder.
+     *  @return Array<PuzzleInfo>
+     */
+    public static function getUGPuzzles() : Array<PuzzleInfo>
     {
-        sys.io.File.saveBytes(Path.join([ puzzleStorage(), Std.string(_info.id) + '.puzzle' ]), compress(_info));
+        var puzzles = new Array<PuzzleInfo>();
+
+        for (file in FileSystem.readDirectory(puzzleStorage()))
+        {
+            if (!FileSystem.isDirectory(file) && Path.extension(file) == 'puzzle')
+            {
+                puzzles.push(load(file));
+            }
+        }
+
+        return puzzles;
     }
 
+    /**
+     *  Serializes and compresses the provided PuzzleInfo class. The file is saved in the puzzle storage.
+     *  The file name is the puzzles id and it's name.
+     *  @param _info - The puzzle to save.
+     */
+    public static function save(_info : PuzzleInfo)
+    {
+        File.saveBytes(Path.join([ puzzleStorage(), Std.string(_info.id) + '-${_info.name}.puzzle' ]), compress(_info));
+    }
+
+    /**
+     *  Decompresses and deserialized the provided puzzle returning a PuzzleInfo object.
+     *  @param _puzzle - The name of the puzzle.
+     *  @return PuzzleInfo
+     */
     public static function load(_puzzle : String) : PuzzleInfo
     {
-        return decompress(sys.io.File.getBytes(Path.join([ puzzleStorage(), _puzzle + '.puzzle' ])));
+        return decompress(File.getBytes(Path.join([ puzzleStorage(), _puzzle ])));
     }
 
     private static function compress(_info : PuzzleInfo) : Bytes
