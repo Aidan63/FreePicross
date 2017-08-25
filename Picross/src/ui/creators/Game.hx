da675d43e33c47a3d02a18b6baac55b91adeaadf
Aidan Lee <aidan.lee63@gmail.com>
2017-08-25 13:36:47 +0100

    Added new ui creator class for the game state.

---
+++ Picross/src/ui/creators/Game.hx
@@ -0,0 +1,13 @@
+package ui.creators;
+
+import luxe.Visual;
+
+class Game
+{
+    public static inline function createHUD() : Visual
+    {
+        var panel = new Visual({});
+
+        return panel;
+    }
+}
