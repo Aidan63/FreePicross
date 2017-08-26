da51bb05d1bea7ec45d504c7e66a0e004b558f5a
Aidan Lee <aidan.lee63@gmail.com>
2017-08-26 16:12:51 +0100

    The game UI is now stored and events connected in the game state.

--- Picross/src/states/game/GameState.hx
+++ Picross/src/states/game/GameState.hx
@@ -3,17 +3,29 @@ package states.game;
 import luxe.States;
 import luxe.Parcel;
 import luxe.Visual;
+import luxe.Vector;
 import luxe.ParcelProgress;
 import luxe.Color;
 import game.PuzzleState;
 import data.PuzzleInfo;
+import utils.Effect;
+using utils.EntityHelper;
 
 class GameState extends State
 {
+    /**
+     *  The current active puzzle info.
+     */
     private var info : PuzzleInfo;
 
+    /**
+     *  The puzzle entity.
+     */
     private var puzzle : Visual;
 
+    /**
+     *  The game UI entity.
+     */
     private var hud : Visual;
 
     override public function onenter<T>(_data : T)
@@ -68,14 +80,70 @@ class GameState extends State
         puzzle.add(new components.CellBreaker    ({ name : 'remove_effect'    }));
 
         utils.Banner.create('Start!', 1);
-        Luxe.timer.schedule(2, function() {
-            puzzle.add(new components.MousePress ({ name : 'mouse' }));
+        Luxe.timer.schedule(2, startPuzzle);
+    }
 
-            hud = ui.creators.Game.createHUD();
-        });
+    private function startPuzzle()
+    {
+        puzzle.add(new components.MousePress ({ name : 'mouse' }));
+
+        hud = ui.creators.Game.createHUD();
+        hud.pos.set_xy(-128, 0);
+        luxe.tween.Actuate.tween(hud.pos, 0.25, { x : 0 });
 
         // Connect listeners to the puzzle.
 
         // Connect listeners to the HUD.
+        hud.findChild('bttn_pause').events.listen('clicked', onPausePressed);
+        hud.findChild('bttn_paintPrimary').events.listen('clicked', onPrimaryPressed);
+        hud.findChild('bttn_paintSecondary').events.listen('clicked', onSecondaryPressed);
+    }
+
+    /**
+     *  Button listener functions
+     */
+
+    /**
+     *  Plays a select effect when pressed. TODO : Actually pause the game.
+     */
+    private function onPausePressed(_)
+    {
+        var pause : Visual = cast hud.findChild('bttn_pause');
+
+        Effect.select(pause.transform.world.pos.clone(), pause.size.clone(), new Vector(12, 12));
+    }
+
+    /**
+     *  Sets the current colour to Primary, slide the selector to the primary paint button and play a select effect.
+     */
+    private function onPrimaryPressed(_)
+    {
+        PuzzleState.color.currentColor = Primary;
+
+        var selector = hud.findChild('bttn_paintSelector');
+        var primary : Visual = cast hud.findChild('bttn_paintPrimary');
+
+        // Slide the selector to this button.
+        if (selector.has('slide')) selector.remove('slide');
+        selector.add(new components.Slider({ name : 'slide', time : 0.25, end : primary.pos, ease : luxe.tween.easing.Quad.easeOut }));
+
+        Effect.select(primary.transform.world.pos.clone(), primary.size.clone(), new Vector(12, 12));
+    }
+
+    /**
+     *  Sets the current colour to Secondary, slide the selector to the secondary paint button and play a select effect.
+     */
+    private function onSecondaryPressed(_)
+    {
+        PuzzleState.color.currentColor = Secondary;
+
+        var selector  = hud.findChild('bttn_paintSelector');
+        var secondary : Visual = cast hud.findChild('bttn_paintSecondary');
+
+        // Slide the selector to this button.
+        if (selector.has('slide')) selector.remove('slide');
+        selector.add(new components.Slider({ name : 'slide', time : 0.25, end : secondary.pos, ease : luxe.tween.easing.Quad.easeOut }));
+
+        Effect.select(secondary.transform.world.pos.clone(), secondary.size.clone(), new Vector(12, 12));
     }
 }
--- Picross/src/ui/creators/Game.hx
+++ Picross/src/ui/creators/Game.hx
@@ -1,12 +1,80 @@
 package ui.creators;
 
 import luxe.Visual;
+import luxe.Text;
+import luxe.Sprite;
+import luxe.Vector;
+import luxe.Color;
+import luxe.components.sprite.SpriteAnimation;
+import game.PuzzleState;
 
 class Game
 {
     public static inline function createHUD() : Visual
     {
-        var panel = new Visual({});
+        var panel = new Visual({
+            name  : 'panel_ui',
+            pos   : new Vector(0, 0),
+            size  : new Vector(128, 720),
+            color : new Color().rgb(0x363636),
+        });
+
+        var pause = new Visual({
+            name     : 'bttn_pause',
+            parent   : panel,
+            pos      : new Vector(0, 0),
+            size     : new Vector(128, 128),
+            geometry : Luxe.draw.texture({
+                texture : Luxe.resources.texture('assets/images/ui/buttonPause.png')
+            })
+        });
+        pause.add(new components.ui.Button({ name : 'button' }));
+        pause.transform.world.auto_decompose = true;
+
+        var paintPrimary = new Visual({
+            name     : 'bttn_paintPrimary',
+            parent   : panel,
+            pos      : new Vector(0, 128),
+            size     : new Vector(128, 128),
+            color    : PuzzleState.color.colors.get(Primary)
+        });
+        paintPrimary.add(new components.ui.Button({ name : 'button' }));
+        paintPrimary.transform.world.auto_decompose = true;
+
+        var paintSecondary = new Visual({
+            name     : 'bttn_paintSecondary',
+            parent   : panel,
+            pos      : new Vector(0, 256),
+            size     : new Vector(128, 128),
+            color    : PuzzleState.color.colors.get(Secondary)
+        });
+        paintSecondary.add(new components.ui.Button({ name : 'button' }));
+        paintSecondary.transform.world.auto_decompose = true;
+
+        var paintSelector = new Sprite({
+            name    : 'bttn_paintSelector',
+            parent  : panel,
+            texture : Luxe.resources.texture('assets/images/ui/paintSelector.png'),
+            pos     : paintPrimary.pos,
+            size    : new Vector(128, 128),
+            origin  : new Vector(0, 0),
+        });
+        paintSelector.transform.world.auto_decompose = true;
+        
+        var anim : SpriteAnimation = paintSelector.add(new SpriteAnimation({ name : 'animation' }));
+        anim.add_from_json_object(Luxe.resources.json('assets/data/animations/paintSelector.json').asset.json);
+        anim.animation = 'idle';
+        anim.play();
+
+        new Text({
+            name : 'label_timer',
+            parent : panel,
+            text : '00:00',
+            point_size : 24,
+            pos : new Vector(64, 448),
+            align : center,
+            align_vertical : center
+        });
 
         return panel;
     }
